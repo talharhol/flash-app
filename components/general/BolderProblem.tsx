c1884b1a1887a2faa012d7705143a1b2f77cc6c5
Tal Har Hol <talharhol@gmail.com>
2024-07-09 22:37:45 +0300

    added option to save bolders to galery

--- components/general/BolderProblem.tsx
+++ components/general/BolderProblem.tsx
@@ -1,6 +1,6 @@
 import { Hold, HoldInterface, HoldType, SortHolds } from "@/DAL/hold";
 import { imageSize } from "../general/SizeContext";
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
 import {
   Image,
   Platform,
@@ -9,14 +9,17 @@ import {
   View,
   useWindowDimensions,
   ImageSourcePropType,
+  ViewProps,
 } from "react-native";
 import Svg from "react-native-svg";
 import Zoomable from "./Zoomable";
 import DrawHold from "./DrawHold";
 import SVGHold from "./SvgHold";
 import { svgZoom } from "@/constants/consts";
+import ViewShot, {captureRef} from "react-native-view-shot";
+import * as MediaLibrary from 'expo-media-library';
 
-const BolderProblem: React.FC<React.ComponentProps<typeof View> & {
+interface BolderProblemProps extends ViewProps {
   wallImage: ImageSourcePropType;
   configuredHolds?: { id: string; svgPath: string; }[];
   existingHolds?: HoldInterface[];
@@ -28,7 +31,18 @@ const BolderProblem: React.FC<React.ComponentProps<typeof View> & {
   onDrawHoldCancel?: () => void;
   onConfiguredHoldClick?: (hold_id: string) => void;
   onHoldClick?: (hold_id: string) => void;
-}> = ({ wallImage, configuredHolds, existingHolds, drawingHoldType, disableMovment, scale, fullScreen, onDrawHoldFinish, onDrawHoldCancel, onConfiguredHoldClick, onHoldClick, ...props }) => {
+}
+
+export interface BolderProblemComponent {
+  exportProblem: () => void;
+}
+
+
+const BolderProblem = forwardRef<BolderProblemComponent, BolderProblemProps>(
+  (
+    { wallImage, configuredHolds, existingHolds, drawingHoldType, disableMovment, scale, fullScreen, onDrawHoldFinish, onDrawHoldCancel, onConfiguredHoldClick, onHoldClick, ...props }
+    , ref
+  ) => {
   const screenDimension = useWindowDimensions();
   const [imageHeight, setImageHeight] = useState(0);
   const [imageWidth, setImageWidth] = useState(screenDimension.width * (scale || 1));
@@ -51,56 +65,80 @@ const BolderProblem: React.FC<React.ComponentProps<typeof View> & {
     });
   }, [wallImage]);
   const zoomableViewRef = useRef<React.ElementRef<typeof Zoomable>>(null);
+  const viewShotRef = useRef<React.ElementRef<typeof ViewShot>>();
+  const captureAndSave = async () => {
+    try {
+      const uri = await captureRef(viewShotRef, {
+        format: 'png',
+        quality: 1,
+      });
+      await MediaLibrary.saveToLibraryAsync(uri);
+      alert('Image saved to gallery!');
+    } catch (error) {
+      console.error('Error capturing and saving image:', error);
+    }
+  };
 
+  useImperativeHandle(ref, () => {
+    return {
+      exportProblem() {
+        captureAndSave();
+      },
+    };
+  }, []);
+  
   return (
     <View {...props} style={[styles.zoomedContainer, { height: fullScreen ? screenDimension.height * (scale || 1) : screenDimension.width * 1.5 * (scale || 1), width: screenDimension.width * (scale || 1) }, props.style]}>
       <imageSize.Provider value={{ width: imageWidth, height: imageHeight }}>
         <Zoomable
           ref={zoomableViewRef}
           disableMovement={!!disableMovment || !!drawingHoldType} maxZoom={20}>
-          <View style={styles.zoomedContent}>
-            {
-              !!drawingHoldType &&
-              <DrawHold
-                onCancel={onDrawHoldCancel}
-                currentHoldType={drawingHoldType}
-                onFinishedDrawingShape={onCreatedHold}
-              />
-            }
-            <View style={{ position: "absolute", zIndex: 1 }}>
-              <Svg
-                viewBox={`0 0 ${imageWidth * svgZoom / (scale || 1)} ${imageHeight * svgZoom / (scale || 1)}`}
-                style={[{ position: "relative", width: imageWidth, height: imageHeight }]}>
-                {
-                  configuredHolds?.sort(SortHolds).map(hold => (
-                    <SVGHold
-                      key={hold.id}
-                      hold={hold}
-                      transparant
-                      onHoldClick={onConfiguredHoldClick}
-                      zoomableViewRef={zoomableViewRef}
-                    />
-                  ))
-                }
-                {
-                  existingHolds?.sort(SortHolds).map((hold) => (
-                    <SVGHold
-                      key={hold.id}
-                      hold={hold}
-                      onHoldClick={onHoldClick}
-                      zoomableViewRef={zoomableViewRef}
-                    />
-                  ))
-                }
-              </Svg>
+          <ViewShot ref={viewShotRef as any} options={{ format: 'png', quality: 1 }}>
+            <View style={styles.zoomedContent}>
+              {
+                !!drawingHoldType &&
+                <DrawHold
+                  onCancel={onDrawHoldCancel}
+                  currentHoldType={drawingHoldType}
+                  onFinishedDrawingShape={onCreatedHold}
+                />
+              }
+              <View style={{ position: "absolute", zIndex: 1 }}>
+                <Svg
+                  viewBox={`0 0 ${imageWidth * svgZoom / (scale || 1)} ${imageHeight * svgZoom / (scale || 1)}`}
+                  style={[{ position: "relative", width: imageWidth, height: imageHeight }]}>
+                  {
+                    configuredHolds?.sort(SortHolds).map(hold => (
+                      <SVGHold
+                        key={hold.id}
+                        hold={hold}
+                        transparant
+                        onHoldClick={onConfiguredHoldClick}
+                        zoomableViewRef={zoomableViewRef}
+                      />
+                    ))
+                  }
+                  {
+                    existingHolds?.sort(SortHolds).map((hold) => (
+                      <SVGHold
+                        key={hold.id}
+                        hold={hold}
+                        onHoldClick={onHoldClick}
+                        zoomableViewRef={zoomableViewRef}
+                      />
+                    ))
+                  }
+                </Svg>
+              </View>
+              <Image style={[styles.problemImage, { width: imageWidth, height: imageHeight }]} source={wallImage} />
             </View>
-            <Image style={[styles.problemImage, { width: imageWidth, height: imageHeight }]} source={wallImage} />
-          </View>
+          </ViewShot>
         </Zoomable>
       </imageSize.Provider>
     </View>
   );
-};
+});
+
 
 export default BolderProblem;
 
--- components/general/BolderProblemPreview.tsx
+++ components/general/BolderProblemPreview.tsx
@@ -1,38 +1,47 @@
-import BolderProblem from "@/components/general/BolderProblem";
+import BolderProblem, { BolderProblemComponent } from "@/components/general/BolderProblem";
 import { ThemedText } from "@/components/general/ThemedText";
 import ThemedView from "@/components/general/ThemedView";
 import { grades } from "@/constants/consts";
-import { HoldType, HoldTypes } from "@/DAL/hold";
 import { Problem } from "@/DAL/problem";
 import { Wall } from "@/DAL/wall";
-import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useRef, useState } from "react";
 import { Image, useWindowDimensions } from "react-native";
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
 
 const BolderProblemPreview: React.FC<React.ComponentProps<typeof ThemedView> & {
     wall: Wall;
     problem: Problem;
 }> = ({ wall, problem, ...props }) => {
     const scale = 0.8;
-
     const screenDimension = useWindowDimensions();
     const width = screenDimension.width * scale;
     const [height, setHeight] = useState(0);
     useEffect(() => {
         Image.getSize(Image.resolveAssetSource(wall.image).uri, (w, h) => setHeight(width / (w || 1) * h));
     }, []);
+    const problemRef = useRef<BolderProblemComponent>(null);
 
     return (
         <ThemedView {...props} style={[{ overflow: "hidden", flexDirection: "column", borderRadius: 8, width: width, height: height }]}>
-            <ThemedView style={{ backgroundColor: "rgba(50, 50, 50, 0.4)", flexDirection: "row", justifyContent: 'space-between', position: "absolute", width: "100%", paddingLeft: 5, paddingRight: 5, zIndex: 1}}>
+            <ThemedView style={{ backgroundColor: "rgba(50, 50, 50, 0.4)", flexDirection: "row", justifyContent: 'space-between', position: "absolute", width: "100%", paddingLeft: 5, paddingRight: 5, zIndex: 1 }}>
                 <ThemedText>{problem.name}</ThemedText>
                 <ThemedText>{grades[problem.grade]}</ThemedText>
             </ThemedView>
             <BolderProblem
+                ref={problemRef}
                 scale={scale}
                 wallImage={wall.image}
                 existingHolds={problem.holds}
                 disableMovment
             />
+            <TouchableWithoutFeedback
+                onPress={() => problemRef.current?.exportProblem()}>
+                <Ionicons
+                    name="code-download-outline" size={50} color={"white"} style={{ zIndex: 1, position: "absolute", bottom: 0, right: 0, marginRight: 5 }} />
+            </TouchableWithoutFeedback>
+
         </ThemedView>
     )
 }
