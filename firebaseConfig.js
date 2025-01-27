7ea2079fc83118887172fa6582aee7f01a407a82
Tal Har Hol <talharhol@gmail.com>
2025-01-27 23:40:45 +0200

    using env vars instead of plain text strings

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,23 +4,15 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD9SdPE2u3vQyDmBGGf0Jr3fnEwCDtvtTI",
-  authDomain: "flash-b9950.firebaseapp.com",
-  projectId: "flash-b9950",
-  storageBucket: "flash-b9950.appspot.com",
-  messagingSenderId: "346499251854",
-  appId: "1:346499251854:web:889ba45df565bd4e46d3df",
-  measurementId: "G-LNLD64P3CF",
-  databaseURL: 'https://project-id.firebaseio.com',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
