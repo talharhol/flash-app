071cd1461135e46042d3a10c7d51895a56354034
Tal Har Hol <talharhol@gmail.com>
2025-01-30 00:48:46 +0200

    added missing edit user modal

---
+++ components/screens/SettingsScreen/EditUserModal.tsx
@@ -0,0 +1,31 @@
+import { ThemedText } from "@/components/general/ThemedText";
+import React, { useState } from "react";
+import { TextInput, View } from "react-native";
+import { User } from "@/DAL/entities/user";
+import BasicModal from "@/components/general/modals/BasicModal";
+import BasicButton from "@/components/general/Buttom";
+
+const EditUserModal: React.FC<React.ComponentProps<typeof BasicModal> & {
+    editUser: (user: User) => void;
+    user: User;
+}> = ({ editUser, user, ...props }) => {
+    const [userName, setUserName] = useState(user.name);
+    return (
+        <BasicModal {...props}>
+            <View style={{ width: "100%", alignItems: "center" }}>
+                <ThemedText darkColor="black" type="defaultSemiBold">edit username</ThemedText>
+                <TextInput value={userName} onChangeText={setUserName} style={{ fontSize: 16, height: 40, width: "80%", borderRadius: 8, borderWidth: 2, backgroundColor: "grey", padding: 10 }} />
+            </View>
+            <View style={{ flexDirection: "row" }}>
+                <BasicButton text="save" color="blue"
+                    onPress={() => {
+                        user.name = userName;
+                        editUser(user)
+                    }
+                    } />
+            </View>
+        </BasicModal>
+    );
+};
+
+export default EditUserModal;
