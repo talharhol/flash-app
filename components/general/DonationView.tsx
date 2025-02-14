8835a0ccad3c45baef5643a5073559bf4203319e
Tal Har Hol <talharhol@gmail.com>
2025-02-14 11:54:06 +0200

    made donation view nicer

--- components/general/DonationView.tsx
+++ components/general/DonationView.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
-import { Button, Image, Linking, StyleSheet, Text, TextInput, View } from "react-native";
-import { useDal } from "@/DAL/DALService";
+import React from "react";
+import { Image, Linking, StyleSheet, View } from "react-native";
 import { ThemedText } from "./ThemedText";
+import BasicButton from "./Button";
+import { Colors } from "@/constants/Colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 const DonationView: React.FC<{
@@ -12,10 +14,24 @@ const DonationView: React.FC<{
     return (
       <View style={styles.container}>
         <Image style={styles.loggo} source={require("../../assets/images/loggo.png")} />
-        <ThemedText type="subtitle" style={styles.loggoText}>Flash app</ThemedText>
-        <ThemedText type="title" style={styles.title}>Please donate</ThemedText>
-        <Button onPress={() => Linking.openURL("https://buymeacoffee.com/flashapp")} title="I want to donate" />
-        <Button onPress={close} title="Close" />
+        <ThemedText type="subtitle" style={[styles.text, { marginBottom: 40 }]}>Flash app</ThemedText>
+        <ThemedText type="default1" style={[styles.text, { marginBottom: 5 }]}>Are you ynjoying the app?</ThemedText>
+        <ThemedText type="title" style={[styles.text, { marginBottom: 20 }]}>Please donate</ThemedText>
+        <ThemedText type="subtitle3" style={styles.text}>This app is given for free</ThemedText>
+        <ThemedText type="subtitle3" style={styles.text}>However, it costs money to maintain</ThemedText>
+        <ThemedText type="subtitle3" style={styles.text}>In order to continue operating we relay on donaitions</ThemedText>
+
+        <BasicButton
+          color={Colors.backgroundDark} selected
+          style={{ marginTop: 40 }}
+          onPress={() => Linking.openURL("https://buymeacoffee.com/flashapp")}
+          text="I want to donate"
+        />
+
+        <MaterialCommunityIcons
+          style={{ position: "absolute", top: 20, right: 20 }}
+          onPress={close} name="close-thick" size={25} color={Colors.backgroundDark}
+        />
       </View>
     );
   };
@@ -33,21 +49,11 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     justifyContent: "center",
+    alignItems: "center",
     padding: 20,
-    backgroundColor: "grey"
+    backgroundColor: Colors.backgroundExtraLite
   },
-  loggoText: {
+  text: {
     textAlign: "center",
-    marginBottom: 40,
-  },
-  title: {
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 15,
   }
 });
